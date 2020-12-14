e74c63378e29e216deea343e9a4c66870e8539a6
alexoidozaver <alexeii.lomets@djangostars.com>
2020-12-14 12:47:04 +0200

    asd

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ def main() -> None:
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("1465697626:AAGs7JZJOeDAS11LzaofYsjlim2O0lh7uPk", use_context=True)
+    updater = Updater("1493365842:AAEDREtIf-93x4pE7HQ9koi-MEQ-0T3Dclo", use_context=True)
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
